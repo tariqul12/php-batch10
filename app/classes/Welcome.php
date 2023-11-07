8e01c5b404433694313f8eb95965cfbb6817a030
tariqul12 <mshimul591@gmail.com>
2023-11-07 19:56:39 +0600

    test

--- app/classes/Welcome.php
+++ app/classes/Welcome.php
@@ -4,7 +4,7 @@
 namespace App\classes;
 class Welcome
 {
-    public $message , $firstNamr,$lastNamr;
+    public $message ,$firstNumber,$secondNumber,$result;
 
     public function __construct()
     {
@@ -12,9 +12,54 @@ class Welcome
     }
     public function index()
     {
-         $this->firstNamr="Tariqul Islam";
-         $this->lastNamr="BITM";
-         echo gettype($this->firstNamr);
+        $this->firstNumber=100;
+        $this->secondNumber= 20;
+
+//        conditional operator
+
+        echo $this->firstNumber > $this->secondNumber;
+//        //assigment operator
+//        echo "<br>";
+//        echo $this->firstNumber += $this->secondNumber;
+//        echo "<br>";
+//        echo $this->firstNumber -= $this->secondNumber;
+//        echo "<br>";
+//        echo $this->firstNumber *= $this->secondNumber;
+//        echo "<br>";
+//        echo $this->firstNumber /= $this->secondNumber;
+//        echo "<br>";
+//        echo $this->firstNumber %= $this->secondNumber;
+//        echo "<br>";
+//        echo $this->firstNumber .= $this->secondNumber;
+
+
+        //aritmetic operator
+//        echo "<br>";
+//        echo ++$this->firstNumber;
+//        echo "<br>";
+//        echo --$this->firstNumber++;
+//        echo "<br>";
+//        echo -$this->firstNumber;
+//        echo "<br>";
+//        echo $this->firstNumber;
+
+
+//        echo "<br>";
+//        echo $this->firstNamr + $this->secondNumber;
+//        echo "<br>";
+//        echo $this->firstNamr - $this->secondNumber;
+//        echo "<br>";
+//        echo $this->firstNamr * $this->secondNumber;
+//        echo "<br>";
+//        echo $this->firstNamr / $this->secondNumber;
+//        echo "<br>";
+//        echo $this->firstNamr % $this->secondNumber;
+
+
+
+//         $this->firstNamr="Tariqul Islam";
+//         $this->lastNamr="BITM";
+//         echo gettype($this->firstNamr);
 //        echo $this->firstNamr." ".$this->lastNamr;
 //        echo $this->message;
     }
